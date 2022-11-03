65469edb3d6fabb5db937bd92db164d09d356d65
a4amber <aachow@princeton.edu>
2022-11-03 11:48:50 -0400

    debug

--- symtablehash.c
+++ symtablehash.c
@@ -79,7 +79,8 @@ that contains no bindings, or NULL if insufficient memory is available.*/
   {
   while(oSymTable->hash[i] != NULL){
     toFree =oSymTable->hash[i];
-    oSymTable->hash[i] =NULL;
+    fprintf( stderr, "this is the Node with key: %s", toFree->key);
+    oSymTable->hash[i] = toFree->next;
     free(toFree->key);
     free(toFree);
   } 
